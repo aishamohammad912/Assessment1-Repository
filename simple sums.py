1feb9cf25d5e9348802c2ddcbe379530c0130acf
aishamohammad912 <aishaatta912@gmail.com>
2024-11-18 11:24:31 +0400

    Add files via upload

---
+++ simple sums.py
@@ -0,0 +1,7 @@
+# Declaring and intializing variables
+num1=8
+num2=10
+# Declaring the third value that stores the sum
+sum_of_numbers=num1+num2
+#printing the sum to console
+print(sum_of_numbers)
