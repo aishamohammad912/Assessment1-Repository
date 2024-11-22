7a71a60f61aa4e59c2daf5d724b0969830385aa0
aishamohammad912 <aishaatta912@gmail.com>
2024-11-22 21:34:00 +0400

    Add files via upload

---
+++ Exercise 4 primitive quiz.py
@@ -0,0 +1,7 @@
+#Asking user for the capital of france
+answer=input("What is the capital of france")
+
+#CHecking if the answer is correct or not
+if answer=="paris":
+    print("That's correct! Welldone! ")
+else:("oops! That's not right.The capital of france is paris")
