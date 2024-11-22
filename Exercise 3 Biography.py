ca4cba3f9725ebe7a49d9f9791a74a4b82af48a2
aishamohammad912 <aishaatta912@gmail.com>
2024-11-22 21:35:20 +0400

    Add files via upload

---
+++ Exercise 3 Biography.py
@@ -0,0 +1,10 @@
+#Storing information iin dictionary
+name="aisha"
+hometown="ajman"
+age=18
+
+#Creating a dictionary
+personal_info={"name":"aisha","hometown":"ajman","age":18}
+
+#printing the values on seperate line using single print ()statement
+print(personal_info["name"],personal_info["hometown"],personal_info["age"],sep="\n")
