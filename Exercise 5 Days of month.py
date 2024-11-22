48005aa55ee56663b812f70c064667cd073843ed
aishamohammad912 <aishaatta912@gmail.com>
2024-11-23 03:42:08 +0400

    Add files via upload

---
+++ Exercise 5 Days of month.py
@@ -0,0 +1,12 @@
+#Creating a dictionary 
+month_days={1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31}
+#january,februray,march,april,may,june,july,august,september,october,november,december
+
+#Asking user for month numbers
+month_number=int(input("Enter the month number:"))
+
+#Checking if the number is valid and print the corresponding number of days
+if 1<=month_number<=12:
+    print(f"The month{month_number}has {month_days[month_number]}days.")
+else:
+    print("Invalid number.Enter a number between 1 and 12.")
