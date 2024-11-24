bc8136ac0511448a231a4e8cf069706449b2a94c
aishamohammad912 <aishaatta912@gmail.com>
2024-11-24 20:10:37 +0400

    Add files via upload

---
+++ Exercise 10 Even and odd.py
@@ -0,0 +1,12 @@
+number=int(input("Enter num :"))
+if number%2==0:
+    print("Even")
+else:
+    print("odd")    
+
+#Use of main function 
+def main():
+#Asking user for input 
+   user_input=int(input("Enter the number:"))
+
+print("number")   
