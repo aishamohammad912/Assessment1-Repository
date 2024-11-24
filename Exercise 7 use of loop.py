bedef31e703a20bcb6c7dc03e6e9c3f7873230be
aishamohammad912 <aishaatta912@gmail.com>
2024-11-24 20:09:22 +0400

    Add files via upload

---
+++ Exercise 7 use of loop.py
@@ -0,0 +1,20 @@
+# use of loop
+#loop the counts up from 0 to 50 in increment  of 1
+for i in range(0,51,1,):
+    print(i)
+
+#loop that count down from 50 to 0 in decrement of 1
+    for i in range(50,-1,-1):
+        print(i)
+
+ #loop that count up from 30 to 50 in increment of 1
+for  i in range(30,51,1):
+    print(i) 
+
+#loop that counts downs from 50 to 10 in decrement of 2
+for i in range(50,9,-2):
+ print(i)
+
+ # loop that count up from 100 to 200  in increment of 5
+ for i in range(100,201,5):
+    print(i)
